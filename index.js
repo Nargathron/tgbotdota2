44fc7a6fd84b0ec2ebae2c3514a1d63a145a965c
Dmitry Boldyrev <dboldyrev@neoflex.ru>
2022-03-30 12:46:42 +0300

    debugging v0.9.7

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('ready', () =>{
 
 client.on("voiceStateUpdate", (oldState, newState) => { // Listeing to the voiceStateUpdate event
     if (newState.channel && !oldState.channel) { // The member connected to a channel.
-        console.log(newState.member.presence.activities)
+        console.log(newState.member.presence.activities.Activity.name)
         axios.post(`${url}${apiToken}/sendMessage`,
              {
                   chat_id: chat_id,
